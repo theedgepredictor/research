3f4ee5789c5047e11ef691e4a2c4136adb684111
Charlie Sergeant <theedgepredictor@gmail.com>
2024-07-23 14:51:48 -0400

    Working apple_health data pipeline

---
+++ research_spikes/workout/consts.py
@@ -0,0 +1,266 @@
+# ME
+HK_ME_DATE_OF_BIRTH = "@HKCharacteristicTypeIdentifierDateOfBirth"
+HK_ME_BIOLOGICAL_SEX = "@HKCharacteristicTypeIdentifierBiologicalSex"
+HK_ME_BLOOD_TYPE = "@HKCharacteristicTypeIdentifierBloodType"
+HK_ME_SKIN_TYPE = "@HKCharacteristicTypeIdentifierFitzpatrickSkinType"
+HK_ME_WHEELCHAIR_USE = "@HKCharacteristicTypeIdentifierWheelchairUse"
+
+# RECORD
+HK_RECORD_ACTIVE_ENERGY_BURNED = "HKQuantityTypeIdentifierActiveEnergyBurned"
+HK_RECORD_APPLE_EXERCISE_TIME = "HKQuantityTypeIdentifierAppleExerciseTime"
+HK_RECORD_APPLE_STAND_HOUR = "HKCategoryTypeIdentifierAppleStandHour"
+HK_RECORD_APPLE_STAND_HOUR_IDLE = "HKCategoryValueAppleStandHourIdle"
+HK_RECORD_APPLE_STAND_HOUR_STOOD = "HKCategoryValueAppleStandHourStood"
+HK_RECORD_APPLE_STAND_TIME = "HKQuantityTypeIdentifierAppleStandTime"
+HK_RECORD_AUDIO_EXPOSURE_EVENT_LOUD_ENVIRONMENT = (
+    "HKCategoryValueAudioExposureEventLoudEnvironment"
+)
+HK_RECORD_BASAL_BODY_TEMPERATURE = "HKQuantityTypeIdentifierBasalBodyTemperature"
+HK_RECORD_BASAL_ENERGY_BURNED = "HKQuantityTypeIdentifierBasalEnergyBurned"
+HK_RECORD_BLOOD_ALCOHOL_CONTENT = "HKQuantityTypeIdentifierBloodAlcoholContent"
+HK_RECORD_BLOOD_GLUCOSE = "HKQuantityTypeIdentifierBloodGlucose"
+HK_RECORD_BLOOD_PRESSURE_DIASTOLIC = "HKQuantityTypeIdentifierBloodPressureDiastolic"
+HK_RECORD_BLOOD_PRESSURE_SYSTOLIC = "HKQuantityTypeIdentifierBloodPressureSystolic"
+HK_RECORD_BODY_FAT_PERCENTAGE = "HKQuantityTypeIdentifierBodyFatPercentage"
+HK_RECORD_BODY_MASS = "HKQuantityTypeIdentifierBodyMass"
+HK_RECORD_BODY_MASS_INDEX = "HKQuantityTypeIdentifierBodyMassIndex"
+HK_RECORD_BODY_TEMPERATURE = "HKQuantityTypeIdentifierBodyTemperature"
+HK_RECORD_CERVICAL_MUCUS_QUALITY_CREAMY = "HKCategoryValueCervicalMucusQualityCreamy"
+HK_RECORD_CERVICAL_MUCUS_QUALITY_DRY = "HKCategoryValueCervicalMucusQualityDry"
+HK_RECORD_CERVICAL_MUCUS_QUALITY_EGG_WHITE = (
+    "HKCategoryValueCervicalMucusQualityEggWhite"
+)
+HK_RECORD_CERVICAL_MUCUS_QUALITY_STICKY = "HKCategoryValueCervicalMucusQualitySticky"
+HK_RECORD_CERVICAL_MUCUS_QUALITY_WATERY = "HKCategoryValueCervicalMucusQualityWatery"
+HK_RECORD_DIETARY_BIOTIN = "HKQuantityTypeIdentifierDietaryBiotin"
+HK_RECORD_DIETARY_CAFFEINE = "HKQuantityTypeIdentifierDietaryCaffeine"
+HK_RECORD_DIETARY_CALCIUM = "HKQuantityTypeIdentifierDietaryCalcium"
+HK_RECORD_DIETARY_CARBOHYDRATES = "HKQuantityTypeIdentifierDietaryCarbohydrates"
+HK_RECORD_DIETARY_CHLORIDE = "HKQuantityTypeIdentifierDietaryChloride"
+HK_RECORD_DIETARY_CHOLESTEROL = "HKQuantityTypeIdentifierDietaryCholesterol"
+HK_RECORD_DIETARY_CHROMIUM = "HKQuantityTypeIdentifierDietaryChromium"
+HK_RECORD_DIETARY_COPPER = "HKQuantityTypeIdentifierDietaryCopper"
+HK_RECORD_DIETARY_ENERGY_CONSUMED = "HKQuantityTypeIdentifierDietaryEnergyConsumed"
+HK_RECORD_DIETARY_FAT_MONOUNSATURATED = (
+    "HKQuantityTypeIdentifierDietaryFatMonounsaturated"
+)
+HK_RECORD_DIETARY_FAT_POLYUNSATURATED = (
+    "HKQuantityTypeIdentifierDietaryFatPolyunsaturated"
+)
+HK_RECORD_DIETARY_FAT_SATURATED = "HKQuantityTypeIdentifierDietaryFatSaturated"
+HK_RECORD_DIETARY_FAT_TOTAL = "HKQuantityTypeIdentifierDietaryFatTotal"
+HK_RECORD_DIETARY_FIBER = "HKQuantityTypeIdentifierDietaryFiber"
+HK_RECORD_DIETARY_FOLATE = "HKQuantityTypeIdentifierDietaryFolate"
+HK_RECORD_DIETARY_IODINE = "HKQuantityTypeIdentifierDietaryIodine"
+HK_RECORD_DIETARY_IRON = "HKQuantityTypeIdentifierDietaryIron"
+HK_RECORD_DIETARY_MAGNESIUM = "HKQuantityTypeIdentifierDietaryMagnesium"
+HK_RECORD_DIETARY_MANGANESE = "HKQuantityTypeIdentifierDietaryManganese"
+HK_RECORD_DIETARY_MOLYBDENUM = "HKQuantityTypeIdentifierDietaryMolybdenum"
+HK_RECORD_DIETARY_NIACIN = "HKQuantityTypeIdentifierDietaryNiacin"
+HK_RECORD_DIETARY_PANTOTHENIC_ACID = "HKQuantityTypeIdentifierDietaryPantothenicAcid"
+HK_RECORD_DIETARY_PHOSPHORUS = "HKQuantityTypeIdentifierDietaryPhosphorus"
+HK_RECORD_DIETARY_POTASSIUM = "HKQuantityTypeIdentifierDietaryPotassium"
+HK_RECORD_DIETARY_PROTEIN = "HKQuantityTypeIdentifierDietaryProtein"
+HK_RECORD_DIETARY_RIBOFLAVIN = "HKQuantityTypeIdentifierDietaryRiboflavin"
+HK_RECORD_DIETARY_SELENIUM = "HKQuantityTypeIdentifierDietarySelenium"
+HK_RECORD_DIETARY_SODIUM = "HKQuantityTypeIdentifierDietarySodium"
+HK_RECORD_DIETARY_SUGAR = "HKQuantityTypeIdentifierDietarySugar"
+HK_RECORD_DIETARY_THIAMIN = "HKQuantityTypeIdentifierDietaryThiamin"
+HK_RECORD_DIETARY_VITAMIN_A = "HKQuantityTypeIdentifierDietaryVitaminA"
+HK_RECORD_DIETARY_VITAMIN_B12 = "HKQuantityTypeIdentifierDietaryVitaminB12"
+HK_RECORD_DIETARY_VITAMIN_B6 = "HKQuantityTypeIdentifierDietaryVitaminB6"
+HK_RECORD_DIETARY_VITAMIN_C = "HKQuantityTypeIdentifierDietaryVitaminC"
+HK_RECORD_DIETARY_VITAMIN_D = "HKQuantityTypeIdentifierDietaryVitaminD"
+HK_RECORD_DIETARY_VITAMIN_E = "HKQuantityTypeIdentifierDietaryVitaminE"
+HK_RECORD_DIETARY_VITAMIN_K = "HKQuantityTypeIdentifierDietaryVitaminK"
+HK_RECORD_DIETARY_WATER = "HKQuantityTypeIdentifierDietaryWater"
+HK_RECORD_DIETARY_ZINC = "HKQuantityTypeIdentifierDietaryZinc"
+HK_RECORD_DISTANCE_CYCLING = "HKQuantityTypeIdentifierDistanceCycling"
+HK_RECORD_DISTANCE_DOWNHILL_SNOW_SPORTS = (
+    "HKQuantityTypeIdentifierDistanceDownhillSnowSports"
+)
+HK_RECORD_DISTANCE_SWIMMING = "HKQuantityTypeIdentifierDistanceSwimming"
+HK_RECORD_DISTANCE_WALKING_RUNNING = "HKQuantityTypeIdentifierDistanceWalkingRunning"
+HK_RECORD_DISTANCE_WHEELCHAIR = "HKQuantityTypeIdentifierDistanceWheelchair"
+HK_RECORD_ELECTRODERMAL_ACTIVITY = "HKQuantityTypeIdentifierElectrodermalActivity"
+HK_RECORD_ENVIRONMENTAL_AUDIO_EXPOSURE = (
+    "HKQuantityTypeIdentifierEnvironmentalAudioExposure"
+)
+HK_RECORD_FLIGHTS_CLIMBED = "HKQuantityTypeIdentifierFlightsClimbed"
+HK_RECORD_FORCED_EXPIRATORY_VOLUME1 = "HKQuantityTypeIdentifierForcedExpiratoryVolume1"
+HK_RECORD_FORCED_VITAL_CAPACITY = "HKQuantityTypeIdentifierForcedVitalCapacity"
+HK_RECORD_HEADPHONE_AUDIO_EXPOSURE = "HKQuantityTypeIdentifierHeadphoneAudioExposure"
+HK_RECORD_HEART_RATE = "HKQuantityTypeIdentifierHeartRate"
+HK_RECORD_HEART_RATE_VARIABILITY_S_D_N_N = (
+    "HKQuantityTypeIdentifierHeartRateVariabilitySDNN"
+)
+HK_RECORD_HEIGHT = "HKQuantityTypeIdentifierHeight"
+HK_RECORD_INHALER_USAGE = "HKQuantityTypeIdentifierInhalerUsage"
+HK_RECORD_INSULIN_DELIVERY = "HKQuantityTypeIdentifierInsulinDelivery"
+HK_RECORD_LEAN_BODY_MASS = "HKQuantityTypeIdentifierLeanBodyMass"
+HK_RECORD_MINDFUL_SESSION = "HKCategoryTypeIdentifierMindfulSession"
+HK_RECORD_NIKE_FUEL = "HKQuantityTypeIdentifierNikeFuel"
+HK_RECORD_NOT_APPLICABLE = "HKCategoryValueNotApplicable"
+HK_RECORD_NUMBER_OF_TIMES_FALLEN = "HKQuantityTypeIdentifierNumberOfTimesFallen"
+HK_RECORD_OVULATION_TEST_RESULT_ESTROGEN_SURGE = (
+    "HKCategoryValueOvulationTestResultEstrogenSurge"
+)
+HK_RECORD_OVULATION_TEST_RESULT_INDETERMINATE = (
+    "HKCategoryValueOvulationTestResultIndeterminate"
+)
+HK_RECORD_OVULATION_TEST_RESULT_LUTEINIZING_HORMONE_SURGE = (
+    "HKCategoryValueOvulationTestResultLuteinizingHormoneSurge"
+)
+HK_RECORD_OVULATION_TEST_RESULT_NEGATIVE = "HKCategoryValueOvulationTestResultNegative"
+HK_RECORD_OXYGEN_SATURATION = "HKQuantityTypeIdentifierOxygenSaturation"
+HK_RECORD_PEAK_EXPIRATORY_FLOW_RATE = "HKQuantityTypeIdentifierPeakExpiratoryFlowRate"
+HK_RECORD_PERIPHERAL_PERFUSION_INDEX = (
+    "HKQuantityTypeIdentifierPeripheralPerfusionIndex"
+)
+HK_RECORD_PUSH_COUNT = "HKQuantityTypeIdentifierPushCount"
+HK_RECORD_RESPIRATORY_RATE = "HKQuantityTypeIdentifierRespiratoryRate"
+HK_RECORD_RESTING_HEART_RATE = "HKQuantityTypeIdentifierRestingHeartRate"
+HK_RECORD_SLEEP_ANALYSIS = "HKCategoryTypeIdentifierSleepAnalysis"
+HK_RECORD_SLEEP_ANALYSIS_ASLEEP = "HKCategoryValueSleepAnalysisAsleep"
+HK_RECORD_SLEEP_ANALYSIS_AWAKE = "HKCategoryValueSleepAnalysisAwake"
+HK_RECORD_SLEEP_ANALYSIS_IN_BED = "HKCategoryValueSleepAnalysisInBed"
+HK_RECORD_STEP_COUNT = "HKQuantityTypeIdentifierStepCount"
+HK_RECORD_SWIMMING_STROKE_COUNT = "HKQuantityTypeIdentifierSwimmingStrokeCount"
+HK_RECORD_TOOTHBRUSHING_EVENT = "HKCategoryTypeIdentifierToothbrushingEvent"
+HK_RECORD_U_V_EXPOSURE = "HKQuantityTypeIdentifierUVExposure"
+HK_RECORD_V_O2_MAX = "HKQuantityTypeIdentifierVO2Max"
+HK_RECORD_WAIST_CIRCUMFERENCE = "HKQuantityTypeIdentifierWaistCircumference"
+HK_RECORD_WALKING_HEART_RATE_AVERAGE = "HKQuantityTypeIdentifierWalkingHeartRateAverage"
+HK_RECORD_WALKING_DOUBLE_SUPPORT_PERCENTAGE = (
+    "HKQuantityTypeIdentifierWalkingDoubleSupportPercentage"
+)
+HK_RECORD_SIX_MINUTE_WALK_TEST_DISTANCE = (
+    "HKQuantityTypeIdentifierSixMinuteWalkTestDistance"
+)
+HK_RECORD_WALKING_SPEED = "HKQuantityTypeIdentifierWalkingSpeed"
+HK_RECORD_WALKING_STEP_LENGTH = "HKQuantityTypeIdentifierWalkingStepLength"
+HK_RECORD_WALKING_ASYMMETRY_PERCENTAGE = (
+    "HKQuantityTypeIdentifierWalkingAsymmetryPercentage"
+)
+
+# ACTIVE SUMMARY
+DATE_COMPONENTS = "@dateComponents"
+ACTIVE_ENERGY_BURNED = "@activeEnergyBurned"
+ACTIVE_ENERGY_BURNED_GOAL = "@activeEnergyBurnedGoal"
+ACTIVE_ENERGY_BURNED_UNIT = "@activeEnergyBurnedUnit"
+APPLE_EXERCISE_TIME = "@appleExerciseTime"
+APPLE_EXERCISE_TIME_GOAL = "@appleExerciseTimeGoal"
+APPLE_STAND_HOURS = "@appleStandHours"
+APPLE_STAND_HOURS_GOAL = "@appleStandHoursGoal"
+
+# METADATA 
+HK_AVERAGE_METS = "HKAverageMETs"
+HK_ELEVATION_ASCENDED = "HKElevationAscended"
+HK_INDOOR_WORKOUT = "HKIndoorWorkout"
+HK_TIME_ZONE = "HKTimeZone"
+HK_WEATHER_HUMIDITY = "HKWeatherHumidity"
+HK_WEATHER_TEMPERATURE = "HKWeatherTemperature"
+HK_WORKOUT_BRAND_NAME = "HKWorkoutBrandName"
+HK_FOOD_MEAL = "HKFoodMeal"
+HK_FOOD_TYPE = "HKFoodType"
+HK_METADATA_KEY_HEART_RATE_MOTION_CONTEXT = "HKMetadataKeyHeartRateMotionContext"
+HK_SWIMMING_STROKE_STYLE = "HKSwimmingStrokeStyle"
+
+# WORKOUT
+WORKOUT_ACTIVITY_TYPE = "@workoutActivityType"
+DURATION = "@duration"
+DURATION_UNIT = "@durationUnit"
+TOTAL_DISTANCE = "@totalDistance"
+TOTAL_DISTANCE_UNIT = "@totalDistanceUnit"
+TOTAL_ENERGY_BURNED = "@totalEnergyBurned"
+TOTAL_ENERGY_BURNED_UNIT = "@totalEnergyBurnedUnit"
+TOTAL_FLIGHTS_CLIMBED = "@totalFlightsClimbed"
+TOTAL_SWIMMING_STROKE_COUNT = "@totalSwimmingStrokeCount"
+
+# WORKOUT TYPES
+HK_WORKOUT_AMERICAN_FOOTBALL = "HKWorkoutActivityTypeAmericanFootball"
+HK_WORKOUT_ARCHERY = "HKWorkoutActivityTypeArchery"
+HK_WORKOUT_AUSTRALIAN_FOOTBALL = "HKWorkoutActivityTypeAustralianFootball"
+HK_WORKOUT_BADMINTON = "HKWorkoutActivityTypeBadminton"
+HK_WORKOUT_BASEBALL = "HKWorkoutActivityTypeBaseball"
+HK_WORKOUT_BASKETBALL = "HKWorkoutActivityTypeBasketball"
+HK_WORKOUT_BOWLING = "HKWorkoutActivityTypeBowling"
+HK_WORKOUT_BOXING = "HKWorkoutActivityTypeBoxing"
+HK_WORKOUT_CLIMBING = "HKWorkoutActivityTypeClimbing"
+HK_WORKOUT_CRICKET = "HKWorkoutActivityTypeCricket"
+HK_WORKOUT_CROSS_TRAINING = "HKWorkoutActivityTypeCrossTraining"
+HK_WORKOUT_CURLING = "HKWorkoutActivityTypeCurling"
+HK_WORKOUT_CYCLING = "HKWorkoutActivityTypeCycling"
+HK_WORKOUT_DANCE = "HKWorkoutActivityTypeDance"
+HK_WORKOUT_DANCE_INSPIRED_TRAINING = "HKWorkoutActivityTypeDanceInspiredTraining"
+HK_WORKOUT_ELLIPTICAL = "HKWorkoutActivityTypeElliptical"
+HK_WORKOUT_EQUESTRIAN_SPORTS = "HKWorkoutActivityTypeEquestrianSports"
+HK_WORKOUT_FENCING = "HKWorkoutActivityTypeFencing"
+HK_WORKOUT_FISHING = "HKWorkoutActivityTypeFishing"
+HK_WORKOUT_FUNCTIONAL_STRENGTH_TRAINING = (
+    "HKWorkoutActivityTypeFunctionalStrengthTraining"
+)
+HK_WORKOUT_GOLF = "HKWorkoutActivityTypeGolf"
+HK_WORKOUT_GYMNASTICS = "HKWorkoutActivityTypeGymnastics"
+HK_WORKOUT_HANDBALL = "HKWorkoutActivityTypeHandball"
+HK_WORKOUT_HIGH_INTENSITY_INTERVAL_TRAINING = (
+    "HKWorkoutActivityTypeHighIntensityIntervalTraining"
+)
+HK_WORKOUT_HIKING = "HKWorkoutActivityTypeHiking"
+HK_WORKOUT_HOCKEY = "HKWorkoutActivityTypeHockey"
+HK_WORKOUT_HUNTING = "HKWorkoutActivityTypeHunting"
+HK_WORKOUT_LACROSSE = "HKWorkoutActivityTypeLacrosse"
+HK_WORKOUT_MARTIAL_ARTS = "HKWorkoutActivityTypeMartialArts"
+HK_WORKOUT_MIND_AND_BODY = "HKWorkoutActivityTypeMindAndBody"
+HK_WORKOUT_MIXED_METABOLIC_CARDIO_TRAINING = (
+    "HKWorkoutActivityTypeMixedMetabolicCardioTraining"
+)
+HK_WORKOUT_OTHER = "HKWorkoutActivityTypeOther"
+HK_WORKOUT_PADDLE_SPORTS = "HKWorkoutActivityTypePaddleSports"
+HK_WORKOUT_PLAY = "HKWorkoutActivityTypePlay"
+HK_WORKOUT_PREPARATION_AND_RECOVERY = "HKWorkoutActivityTypePreparationAndRecovery"
+HK_WORKOUT_RACQUETBALL = "HKWorkoutActivityTypeRacquetball"
+HK_WORKOUT_ROWING = "HKWorkoutActivityTypeRowing"
+HK_WORKOUT_RUGBY = "HKWorkoutActivityTypeRugby"
+HK_WORKOUT_RUNNING = "HKWorkoutActivityTypeRunning"
+HK_WORKOUT_SAILING = "HKWorkoutActivityTypeSailing"
+HK_WORKOUT_SKATING_SPORTS = "HKWorkoutActivityTypeSkatingSports"
+HK_WORKOUT_SNOW_SPORTS = "HKWorkoutActivityTypeSnowSports"
+HK_WORKOUT_SOCCER = "HKWorkoutActivityTypeSoccer"
+HK_WORKOUT_SOFTBALL = "HKWorkoutActivityTypeSoftball"
+HK_WORKOUT_SQUASH = "HKWorkoutActivityTypeSquash"
+HK_WORKOUT_STAIR_CLIMBING = "HKWorkoutActivityTypeStairClimbing"
+HK_WORKOUT_SURFING_SPORTS = "HKWorkoutActivityTypeSurfingSports"
+HK_WORKOUT_SWIMMING = "HKWorkoutActivityTypeSwimming"
+HK_WORKOUT_TABLE_TENNIS = "HKWorkoutActivityTypeTableTennis"
+HK_WORKOUT_TENNIS = "HKWorkoutActivityTypeTennis"
+HK_WORKOUT_TRACK_AND_FIELD = "HKWorkoutActivityTypeTrackAndField"
+HK_WORKOUT_TRADITIONAL_STRENGTH_TRAINING = (
+    "HKWorkoutActivityTypeTraditionalStrengthTraining"
+)
+HK_WORKOUT_VOLLEYBALL = "HKWorkoutActivityTypeVolleyball"
+HK_WORKOUT_WALKING = "HKWorkoutActivityTypeWalking"
+HK_WORKOUT_WATER_FITNESS = "HKWorkoutActivityTypeWaterFitness"
+HK_WORKOUT_WATER_POLO = "HKWorkoutActivityTypeWaterPolo"
+HK_WORKOUT_WATER_SPORTS = "HKWorkoutActivityTypeWaterSports"
+HK_WORKOUT_WRESTLING = "HKWorkoutActivityTypeWrestling"
+HK_WORKOUT_YOGA = "HKWorkoutActivityTypeYoga"
+
+HK_CONSTANTS = {
+    HK_RECORD_NOT_APPLICABLE: 0,
+    HK_RECORD_APPLE_STAND_HOUR_STOOD: 0,
+    HK_RECORD_APPLE_STAND_HOUR_IDLE: 1,
+    HK_RECORD_AUDIO_EXPOSURE_EVENT_LOUD_ENVIRONMENT: 1,
+    HK_RECORD_CERVICAL_MUCUS_QUALITY_DRY: 1,
+    HK_RECORD_CERVICAL_MUCUS_QUALITY_STICKY: 2,
+    HK_RECORD_CERVICAL_MUCUS_QUALITY_CREAMY: 3,
+    HK_RECORD_CERVICAL_MUCUS_QUALITY_WATERY: 4,
+    HK_RECORD_CERVICAL_MUCUS_QUALITY_EGG_WHITE: 5,
+    HK_RECORD_OVULATION_TEST_RESULT_NEGATIVE: 1,
+    HK_RECORD_OVULATION_TEST_RESULT_LUTEINIZING_HORMONE_SURGE: 2,
+    HK_RECORD_OVULATION_TEST_RESULT_INDETERMINATE: 3,
+    HK_RECORD_OVULATION_TEST_RESULT_ESTROGEN_SURGE: 4,
+    HK_RECORD_SLEEP_ANALYSIS_IN_BED: 0,
+    HK_RECORD_SLEEP_ANALYSIS_ASLEEP: 1,
+    HK_RECORD_SLEEP_ANALYSIS_AWAKE: 2,
+}
